8b7eea581b4bdea64c4988b7c3bd321c7b2cb5cf
Cecile! <i_never_read_my_stuff@hotmail.com>
2016-02-22 21:00:45 -0800

    pyplot still eludes me

---
+++ sandbox/plotting.py
@@ -0,0 +1,66 @@
+## refhttp://matplotlib.org/users/pyplot_tutorial.html
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot([1,2,3,4], [2, 4, 1, 8], 'ro')
+plt.ylabel('some numbers')
+plt.axis([0, 6, 0, 20])  # [xmin, xmax, ymin, ymax] and 'viewport' (?)
+
+t = np.arange(0., 5., 0.2)
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+
+x = np.arange(len(t))
+y = (2*t)**3
+plt.plot(x, y, linewidth=4.0)
+# tuple unpacking with 'line' (?)
+line, = plt.plot(x, y, '-')
+line.set_antialiased(True)  # I don't notice the difference True/False
+
+plt.show()
+
+# ------------------------------------------------------------- #
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+# -------------------------------------------------------------- #
+plt.figure(1)                # the first figure
+plt.subplot(211)             # the first subplot in the first figure
+plt.plot([1, 2, 3])
+plt.subplot(212)             # the second subplot in the first figure
+plt.plot([4, 5, 6])
+
+
+plt.figure(2)                # a second figure
+plt.plot([4, 5, 6])          # creates a subplot(111) by default
+
+plt.figure(1)                # figure 1 current; subplot(212) still current
+plt.subplot(211)             # make subplot(211) in figure1 current
+plt.title('Easy as 1, 2, 3') # subplot 211 title
+plt.show()
+# ---------------------------------------- #
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
