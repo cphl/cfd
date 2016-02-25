beca9a6053044223e3f82c34f89d4647bed58cb0
Cecile! <cepuuher@sdf.org>
2016-02-24 21:55:53 -0800

    Step 6: vectorized implementation

--- 12stepcode/linear_convection_2d.py
+++ 12stepcode/linear_convection_2d.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-12 steps: step 5, 6
+12 steps: step 5, 6 is converting to vectorized
 # If IPython console is open in Spyder/Anaconda it's not happy.
 # But it'll run if starting from WITHOUT IPython console open, IN Spyder
 # Does not work at terminal with either python or ipython :(
@@ -47,20 +47,33 @@ surf = ax.plot_surface(X, Y, u[:])
 u = numpy.ones((ny, nx))
 u[.5/dy:1/dy+1, .5/dx:1/dx+1] = 2
 
+#for n in range(nt+1):  # loop across number of time steps
+#    un = u.copy()
+#    row, col = u.shape
+#    for j in range(1, row):
+#        for i in range(1, col):
+#            u[j, i] = un[j, i] - (c*dt/dx*(un[j, i] - un[j, i-1])) \
+#                - (c*dt/dy*(un[j, i] - un[j-1, i]))
+#            u[0, :] = 1
+#            u[-1, :] = 1
+#            u[:, 0] = 1
+#            u[:, -1] = 1
+
+# --- vectorized implementation ---#
+u = numpy.ones((ny, nx))
+u[.5/dy:1/dy+1, .5/dx:1/dx+1] = 2
+
 for n in range(nt+1):  # loop across number of time steps
     un = u.copy()
-    row, col = u.shape
-    for j in range(1, row):
-        for i in range(1, col):
-            u[j, i] = un[j, i] - (c*dt/dx*(un[j, i] - un[j, i-1])) \
-                - (c*dt/dy*(un[j, i] - un[j-1, i]))
-            u[0, :] = 1
-            u[-1, :] = 1
-            u[:, 0] = 1
-            u[:, -1] = 1
+    u[1:, 1:] = un[1:, 1:] - (c*dt/dx*(un[1:, 1:] - un[1:, 0:-1])) -\
+        (c*dt/dy*(un[1:, 1:] - un[0:-1, 1:]))
+    u[0, :] = 1
+    u[-1, :] = 1
+    u[:, 0] = 1
+    u[:, -1] = 1
 
 fig = pyplot.figure(figsize=(11, 7), dpi=100)
 ax = fig.gca(projection='3d')
 surf2 = ax.plot_surface(X, Y, u[:])
 pyplot.show()
-# --- End of Step 5, Start Step 6 from here --- #
+# --- End of Step 5 and Step 6 --- #
