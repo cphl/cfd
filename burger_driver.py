7e7585ab677a85c9ccfc8803c7dbff03c20c38a8
Cecile! <cepuuher@sdf.org>
2016-03-20 23:07:45 -0700

    Plotting a donut

--- burger_driver.py
+++ burger_driver.py
@@ -12,9 +12,9 @@ from matplotlib import pyplot
 
 
 # Call the function with these parameters
-nx = 41
-ny = 41
-nt = 200  # start smaller while creating 3d arrays to hold all solutions
+nx = 61
+ny = 61
+nt = 800  # start smaller while creating 3d arrays to hold all solutions
 sigma = 0.0009
 nu = 0.01  # TODO: find different nu that will cause effects. Original: 0.01
 area = [3, 2]  # original was 2 x 2
@@ -31,33 +31,36 @@ v0 = numpy.ones((ny, nx))
 #u0a[.5/dy:1/dy+1, .5/dx:1/dx+1] = 2  # hat func IC
 #v0a[.5/dy:1/dy+1, .5/dx:1/dx+1] = 2  # hat func IC
 #
-#TODO: make different initial conditions... something is weird about these...
-u0b = u0.copy()
-v0b = v0.copy()
-u0b[0.2/dy: 0.3/dy, 0.2/dx: 0.3/dx] = 20
-u0b[0.9/dy: 1/dy+3, 0.9/dx+20: 1/dx+23] = 20  # this is the skinny one, i think
-##u0b[0:4, 0:6] = 2
-v0b[0.5/dy: 1/dy+1, 0.5/dx: 1/dx+1] = 2  # hat func IC
 
 
-#
-## donut
-#u0c = u0.copy()
-#v0c = v0.copy()
-#u0c[.5/dy:1/dy+1, .5/dx:1/dx+1] = 2  # hat func IC
-#u0c[.7/dy:0.8/dy+1, .7/dx:0.8/dx+1] = 1  # hat hole IC
-#v0c[.5/dy+5:1/dy+6, .5/dx+5:1/dx+6] = 2  # hat func IC, make non-overlap with u
+##TODO: make different initial conditions... This one just playing around
+#u0b = u0.copy()
+#v0b = v0.copy()
+#u0b[0.2/dy: 0.3/dy, 0.2/dx: 0.3/dx] = 20
+#u0b[0.9/dy: 1/dy, 0.9/dx: 1/dx] = 20
+###u0b[0:4, 0:6] = 2
+#v0b[0.5/dy: 1/dy+1, 0.5/dx: 1/dx+1] = 2  # hat func IC
+#v0b[0.2/dy: 0.3/dy, 0.2/dx: 0.3/dx] = 2  # add junk, see what happens
 
 
-u_soln, v_soln = bgr.solve_burgers(u0b, v0b, area, nx, ny, nt, sigma, nu)
+# donut
+u0c = u0.copy()
+v0c = v0.copy()
+
+u0c[0.5/dy+5: 1/dy+6, 0.5/dx+5: 1/dx+6] = 2  # hat func IC
+v0c[0.5/dy: 1/dy, 0.5/dx: 1/dx] = 3  # hat func IC
+v0c[0.6/dy: 0.9/dy, 0.6/dx: 0.9/dx] = 1  # hat hole IC
+
+
+u_soln, v_soln = bgr.solve_burgers(u0c, v0c, area, nx, ny, nt, sigma, nu)
 
 # to inspect final solution:
-u_final = u_soln[:,:,-1]
-v_final = v_soln[:,:,-1]
+#u_final = u_soln[:,:,-1]
+#v_final = v_soln[:,:,-1]
 
 # plot surface: helpful if a feature is too skinny in one dimension
-bgr.plot2D(u_soln, v_soln, -1)  # -1 gives the final time
-pyplot.show(block=False)
+#bgr.plot2D(u_soln, v_soln, -1)  # -1 gives the final time
+#pyplot.show(block=False)
 
 # plot flat with colourmap
 bgr.plot2D_flat(u_soln, v_soln, 0)
